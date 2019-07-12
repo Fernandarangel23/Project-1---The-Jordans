9fbcd25cb227bead1c8b34b4953452d8094fc238
Fernandarangel23 <48704755+Fernandarangel23@users.noreply.github.com>
2019-07-11 20:41:28 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 api_key_NYT = "nHoMcuOC7ZsGQBVEBrKt6eLqVE06Attv"
 api_key_weather = '028101d361526030bd8c6b3d91ede15d'
 api_key_NBA = "50718737c0mshe75da4de360b64fp174606jsn214aa1a3bcb9"
+api_key_NBA_fer = "319674375dmsh748dabd60d5fa91p111fdbjsnf7f52642baeb"
